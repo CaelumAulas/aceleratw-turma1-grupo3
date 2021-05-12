2f93485fc320bb53394ec75e9abce22f4007bf1c
Kae Uchoa <kaeuchoas@gmail.com>
2021-05-12 16:04:19 -0300

    tests: create file for roundavatar test

---
+++ lata-velha-app/src/components/RoundAvatar/tests/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { screen } from '@testing-library/react'
+import RoundAvatar from '..';
+import renderWithRouter from '../../../utils/test-utils';
+
+describe('<RoundAvatar />', () => {
+
+  it('should have an img tag', () => {
+    const route = '/usuarios'
+  });
+
+  it('should have an alt attribute', () => {
+
+  });
+});
